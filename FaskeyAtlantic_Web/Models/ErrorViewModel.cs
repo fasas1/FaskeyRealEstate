bdb225004e3d7344e85826965d47bda1291767bb
Fasasi Qasim <fasaqa1000@gmail.com>
2023-12-27 11:06:09 +0100

    first commit

---
+++ FaskeyAtlantic_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaskeyAtlantic_Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
